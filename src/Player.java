216e5666cb92d4c79ac7f8036ea854c0384002a7
mohan-333 <mohan.reddy@sigmoidanalytics.com>
2024-06-30 07:21:12 +0530

    Implement Player class with health, strength, and attack attributes

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+
+
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public int rollDice() {
+        return (int) (Math.random() * 6) + 1;
+    }
+}
